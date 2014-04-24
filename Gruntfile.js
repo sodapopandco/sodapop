d9af4a716a98f381b87220fc6bc7e1efe2d41bae
James Goode <james@sodapopandco.com>
2014-04-24 08:14:15 +0200

    Add empty tasks for managing CSS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
+    autoprefixer: {
+
+    },
+    clean: {
+
+    },
+    csslint: {
+
+    },
+    cssmin: {
+
+    },
+    sass: {
+
+    },
     watch: {
 
     }
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
