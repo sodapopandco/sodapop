d2a3a4456087d5d74687ff4183b1d2fb4e4a0265
James Goode <james@sodapopandco.com>
2014-09-18 21:26:26 +1000

    Add some gulp resources to work with Sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
 gulp.task('default', function() {
   // place code for your default task here
