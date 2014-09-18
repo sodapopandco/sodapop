3b2d4c56d3341ca2b60da7babb77b8912665a3eb
James Goode <james@sodapopandco.com>
2014-09-18 21:43:05 +1000

    Add some tasks to compile Sass files
    Also automatically add vendor prefixes for CSS properties, minify the
    CSS and add a ‘.min’ suffix to the compiled file name.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
+    minifyCSS = require('gulp-minify-css'),
     rename = require('gulp-rename');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('css', function () {
+  return gulp.src('source/_assets/styles/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('public/assets/styles'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('public/assets/styles'))
+});
+
+gulp.task('default', ['css'], function() {
+  gulp.watch("source/**/*.scss", ['css']);
 });
