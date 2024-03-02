7b0a68d69853fb49642ef91772f1ae8bf312e3cd
James Goode <james@sodapopandco.com>
2024-03-02 17:05:03 -0500

    Add a basic config file

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  // Set directories to pass through to the public folder
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    // Set input and output directories
+    dir: {
+      input: "./",
+      output: "./public",
+    },
+  };
+};
