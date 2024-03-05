1ebd236236a1e25e63ea2eb049d4831036715d7b
James Goode <james@sodapopandco.com>
2024-03-05 13:21:23 -0500

    Fix links at the bottom of the page

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,19 +1,28 @@
 // https://sushkelly.co.uk/blog/2024-01-15-obfuscated-email-on-netlify/
 
 // Fetch the email address on click
-document.getElementById('email-button').addEventListener('click', function() {
-  fetch('/.netlify/functions/email')
-    .then(response => response.text())
-    .then(mailtoLink => {
-      window.location.href = mailtoLink;
-    });
+document.querySelectorAll('.email-button').forEach(function(button) {
+  button.addEventListener('click', function() {
+    event.preventDefault();
+
+    fetch('/.netlify/functions/email')
+      .then(response => response.text())
+      .then(mailtoLink => {
+        window.location.href = mailtoLink;
+      });
+  });
 });
 
+
 // Fetch the phone number on click
-document.getElementById('sms-button').addEventListener('click', function() {
-  fetch('/.netlify/functions/phone')
-    .then(response => response.text())
-    .then(smsLink => {
-      window.location.href = smsLink;
-    });
+document.querySelectorAll('.sms-button').forEach(function(button) {
+  button.addEventListener('click', function() {
+    event.preventDefault();
+
+    fetch('/.netlify/functions/sms')
+      .then(response => response.text())
+      .then(mailtoLink => {
+        window.location.href = mailtoLink;
+      });
+  });
 });
